fdfad28597dd14198b4505d421ce1ca8506720eb
Pablo Ulloa <pablo.ulloac@usach.cl>
2023-04-11 00:02:03 -0400

    adding labels by files and lines changed (#1)

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import {info, setFailed} from '@actions/core'
+import {context} from '@actions/github'
+import {createOrUpdateLabels} from './labels/handler'
+import {getLabelForChangeSize, parseLabels} from './labels/utils'
+import {pullRequestHandler} from './pullRequest/handler'
+import {Label} from './types'
+import {input, client, prNumber} from './proxy'
+
+async function handleSizeLabels(changeSize: number, labelsString: string) {
+  const complexityLabel: Label = getLabelForChangeSize(changeSize, labelsString)
+  if (!pullRequestHandler.labelCurrentlyInPullRequest(complexityLabel)) {
+    const possibleLabels: Label[] = parseLabels(labelsString)
+    await pullRequestHandler.cleanLabels(possibleLabels)
+    await pullRequestHandler.addLabel(complexityLabel)
+  }
+}
+
+async function run(): Promise<void> {
+  try {
+    if (prNumber === 0) {
+      throw new Error(`This Action is only supported on 'pull_request' events.`)
+    }
+    await pullRequestHandler.loadCurrentLabels()
+    const {title, body, additions, deletions, changed_files} = (
+      await client.rest.pulls.get({...context.repo, pull_number: prNumber})
+    ).data
+    await createOrUpdateLabels(input.complexityLabels)
+    await createOrUpdateLabels(input.sizeLabels)
+    if (input.useComplexityLabels) {
+      await handleSizeLabels(additions + deletions, input.complexityLabels)
+    }
+    if (input.useSizeLabels) {
+      await handleSizeLabels(changed_files, input.sizeLabels)
+    }
+    if (input.useBodyMetadataLabels) {
+      info(`${body}`)
+    }
+    if (input.useTitleMetadataLabels) {
+      info(`${title}`)
+    }
+  } catch (error: any) {
+    setFailed(error.message)
+  }
+}
+
+run()
---
+++ src/labels/handler.ts
@@ -0,0 +1,57 @@
+import {info} from '@actions/core'
+import {context} from '@actions/github'
+import {Label} from '../types'
+import {client} from '../proxy'
+import {isLabelPresent, parseLabels} from './utils'
+
+const logLabel = (label: Label): void =>
+  info(`label: ${label.name}, color: ${label.color}`)
+const logLabels = (labels: Label[]): void => {
+  for (const label of labels) {
+    logLabel(label)
+  }
+}
+
+async function getLabelsForRepo(): Promise<Label[]> {
+  const {data} = await client.rest.issues.listLabelsForRepo(context.repo)
+  return data.map(
+    label =>
+      ({
+        name: label.name,
+        description: label.description,
+        color: label.color
+      } as Label)
+  )
+}
+
+async function createLabelIfNotPresent(
+  label: Label,
+  currentLabels: Label[]
+): Promise<Label | undefined> {
+  if (!isLabelPresent(label, currentLabels)) {
+    const {data} = await client.rest.issues.createLabel({
+      ...context.repo,
+      name: label.name,
+      color: label.color,
+      description: label.description || undefined
+    })
+    return new Label(data.name, data.color, data.description)
+  }
+  return undefined
+}
+
+export async function createOrUpdateLabels(
+  formattedLabels: string
+): Promise<Label[]> {
+  const availableLabelsAtBegining: Label[] = await getLabelsForRepo()
+  const requiredLabels: Label[] = parseLabels(formattedLabels)
+  info('Current Labels:')
+  logLabels(availableLabelsAtBegining)
+  info('Required Labels:')
+  logLabels(requiredLabels)
+  for (const label of requiredLabels) {
+    createLabelIfNotPresent(label, availableLabelsAtBegining)
+  }
+  const availableLabelsAtEnd: Label[] = await getLabelsForRepo()
+  return availableLabelsAtEnd
+}
