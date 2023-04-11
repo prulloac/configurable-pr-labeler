65659e39c43d00d36333377c4c98f702312cf068
Pablo Ulloa <pablo.ulloac@usach.cl>
2023-04-11 02:49:42 +0000

    testing

--- src/labels/handler.ts
+++ src/labels/handler.ts
@@ -1,7 +1,16 @@
+import {info} from '@actions/core'
 import {context} from '@actions/github'
 import {Label} from '../types'
 import {client} from '../proxy'
 import {parseLabels} from './formatter'
+
+const logLabel = (label: Label): void =>
+  info(`label: ${label.name}, color: ${label.color}`)
+const logLabels = (labels: Label[]): void => {
+  for (const label of labels) {
+    logLabel(label)
+  }
+}
 
 async function getLabelsForRepo(): Promise<Label[]> {
   const {data} = await client.rest.issues.listLabelsForRepo(context.repo)
@@ -38,6 +47,10 @@ export async function createOrUpdateLabels(
 ): Promise<Label[]> {
   const availableLabelsAtBegining: Label[] = await getLabelsForRepo()
   const requiredLabels: Label[] = parseLabels(formattedLabels)
+  info('Current Labels:')
+  logLabels(availableLabelsAtBegining)
+  info('Required Labels:')
+  logLabels(requiredLabels)
   for (const label of requiredLabels) {
     createLabelIfNotPresent(label, availableLabelsAtBegining)
   }
