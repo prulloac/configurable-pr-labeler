40ac2f6816e5a32fad00e7d95dcb4af0caeda757
Pablo Ulloa <pablo.ulloac@usach.cl>
2023-04-16 23:44:08 +0000

    test

--- src/index.ts
+++ src/index.ts
@@ -1,89 +1,78 @@
 import {getInput, info, setFailed, warning} from '@actions/core'
 import {context, getOctokit} from '@actions/github'
 import {PullRequest} from './pullRequest/PullRequest'
-import {ConditionalLabel, RepoLabel} from './labels/types'
+import {ClientType, ConditionalLabel, RepoLabel} from './types'
 import {load as loadYaml} from 'js-yaml'
 import {unemojify} from 'node-emoji'
 
-type ClientType = ReturnType<typeof getOctokit>
-
 function parseConfigObject(configObject: any): ConditionalLabel[] {
-  const config: ConditionalLabel[] = new Array<ConditionalLabel>()
-  if (!Object.keys(configObject).includes('labels')) {
-    warning(`input readed as: ${JSON.stringify(configObject)}`)
-    throw Error('Configuration does not have labels key')
-  }
-  for (const label of configObject.labels) {
-    const keys = Object.keys(label)
-    if (keys.includes('name') && keys.includes('conditions')) {
-      config.push(label as ConditionalLabel)
-    } else {
-      warning(`input readed as: ${JSON.stringify(label)}`)
-      throw Error('ConditionalLabel not instantiable')
-    }
-  }
-  return config
+	const config: ConditionalLabel[] = new Array<ConditionalLabel>()
+	if (!Object.keys(configObject).includes('labels')) {
+		warning(`input readed as: ${JSON.stringify(configObject)}`)
+		throw Error('Configuration does not have labels key')
+	}
+	for (const label of configObject.labels) {
+		const keys = Object.keys(label)
+		if (keys.includes('name') && keys.includes('conditions')) {
+			config.push(label as ConditionalLabel)
+		} else {
+			warning(`input readed as: ${JSON.stringify(label)}`)
+			throw Error('ConditionalLabel not instantiable')
+		}
+	}
+	return config
 }
 
-async function fetchConfig(
-  client: ClientType,
-  configFilePath: string
-): Promise<ConditionalLabel[]> {
-  const response: any = await client.rest.repos.getContent({
-    repo: context.repo.repo,
-    owner: context.repo.owner,
-    ref: context.sha,
-    path: configFilePath
-  })
-  const configString: string = Buffer.from(
-    response.data.content,
-    response.data.encoding
-  ).toString()
-  const configObject: any = loadYaml(configString)
-  return parseConfigObject(configObject)
+async function fetchConfig(client: ClientType, configFilePath: string): Promise<ConditionalLabel[]> {
+	const response: any = await client.rest.repos.getContent({
+		repo: context.repo.repo,
+		owner: context.repo.owner,
+		ref: context.sha,
+		path: configFilePath
+	})
+	const configString: string = Buffer.from(response.data.content, response.data.encoding).toString()
+	const configObject: any = loadYaml(configString)
+	return parseConfigObject(configObject)
 }
 
 async function loadPullRequestData(client: ClientType): Promise<PullRequest> {
-  const pullRequest: PullRequest = new PullRequest(client)
-  await pullRequest.load()
-  return pullRequest
+	const pullRequest: PullRequest = new PullRequest(client)
+	await pullRequest.load()
+	return pullRequest
 }
 
 async function syncLabels(client: ClientType, config: ConditionalLabel[]) {
-  const {data} = await client.rest.issues.listLabelsForRepo({...context.repo})
-  const currentLabels = data.map(repoLabel => repoLabel.name)
-  const uniqueEntries = new Set(
-    config.map(conditionalLabel => {
-      const labelName = unemojify(conditionalLabel.name)
-      return {
-        name: labelName,
-        color: conditionalLabel.color?.replace('#', '') || undefined,
-        description: conditionalLabel.description || undefined
-      } as RepoLabel
-    })
-  )
-  for (const label of uniqueEntries) {
-    if (currentLabels.includes(label.name)) {
-      continue
-    }
-    info(`creating label: ${label.name} with : ${JSON.stringify(label)}`)
-    await client.rest.issues.createLabel({...context.repo, ...label})
-  }
+	const {data} = await client.rest.issues.listLabelsForRepo({...context.repo})
+	const currentLabels = data.map(repoLabel => repoLabel.name)
+	const uniqueEntries = new Set(
+		config.map(conditionalLabel => {
+			const labelName = unemojify(conditionalLabel.name)
+			return {
+				name: labelName,
+				color: conditionalLabel.color?.replace('#', '') || undefined,
+				description: conditionalLabel.description || undefined
+			} as RepoLabel
+		})
+	)
+	for (const label of uniqueEntries) {
+		if (currentLabels.includes(label.name)) {
+			continue
+		}
+		info(`creating label: ${label.name} with : ${JSON.stringify(label)}`)
+		await client.rest.issues.createLabel({...context.repo, ...label})
+	}
 }
 
 async function run(): Promise<void> {
-  try {
-    const client: ClientType = getOctokit(getInput('token', {required: true}))
-    const config: ConditionalLabel[] = await fetchConfig(
-      client,
-      getInput('configuration_path', {required: false})
-    )
-    await syncLabels(client, config)
-    const pullRequest: PullRequest = await loadPullRequestData(client)
-    await pullRequest.apply(config)
-  } catch (error: any) {
-    setFailed(error.message)
-  }
+	try {
+		const client: ClientType = getOctokit(getInput('token', {required: true}))
+		const config: ConditionalLabel[] = await fetchConfig(client, getInput('configuration_path', {required: false}))
+		await syncLabels(client, config)
+		const pullRequest: PullRequest = await loadPullRequestData(client)
+		await pullRequest.apply(config)
+	} catch (error: any) {
+		setFailed(error.message)
+	}
 }
 
 run()
--- src/pullRequest/PullRequest.ts
+++ src/pullRequest/PullRequest.ts
@@ -1,96 +1,88 @@
-import {context, getOctokit} from '@actions/github'
-import {
-  Condition,
-  Conditions,
-  ConditionalLabel,
-  RepoLabel
-} from '../labels/types'
+import {context} from '@actions/github'
+import {Condition, Conditions, ConditionalLabel, RepoLabel, ClientType} from '../types'
 
-type ClientType = ReturnType<typeof getOctokit>
 interface FilesChanged {
-  files: string[]
-  quantity: number
+	files: string[]
+	quantity: number
 }
 
 export class PullRequest {
-  number!: number
-  title!: string
-  body!: string
-  filesChanged: FilesChanged = {quantity: 0, files: []}
-  labels!: RepoLabel[]
-  linesChanged!: number
-  mergeable!: boolean
-  rebaseable!: boolean
-  author!: string
-  client: ClientType
+	number!: number
+	title!: string
+	body!: string
+	filesChanged: FilesChanged = {quantity: 0, files: []}
+	labels!: RepoLabel[]
+	linesChanged!: number
+	mergeable!: boolean
+	rebaseable!: boolean
+	author!: string
+	client: ClientType
 
-  constructor(client: ClientType) {
-    this.client = client
-    if (!context.payload.pull_request) {
-      throw Error(
-        'Cannot instantiate PullRequest if context is not pull_request'
-      )
-    }
-    this.number = context.payload.pull_request.number
-  }
+	constructor(client: ClientType) {
+		this.client = client
+		if (!context.payload.pull_request) {
+			throw Error('Cannot instantiate PullRequest if context is not pull_request')
+		}
+		this.number = context.payload.pull_request.number
+	}
 
-  async load() {
-    const {data: metaData} = await this.client.rest.pulls.get({
-      ...context.repo,
-      pull_number: this.number
-    })
-    this.body = `${metaData.body}`
-    this.title = metaData.title
-    this.linesChanged = metaData.additions + metaData.deletions
-    this.labels = metaData.labels.map(v => ({
-      name: v.name,
-      color: v.color,
-      description: v.description || undefined
-    }))
-    this.mergeable = metaData.mergeable ?? false
-    this.rebaseable = metaData.rebaseable ?? false
-    this.filesChanged.quantity = metaData.changed_files
-    const {data: commitData} = await this.client.rest.pulls.listCommits({
-      ...context.repo,
-      pull_number: this.number
-    })
-    this.author = `${commitData[0].committer?.login}`
-    const {data: filesData} = await this.client.rest.pulls.listFiles({
-      ...context.repo,
-      pull_number: this.number
-    })
-    this.filesChanged.files = filesData.map(fileData => fileData.filename)
-  }
+	async load() {
+		const {data: metaData} = await this.client.rest.pulls.get({
+			...context.repo,
+			pull_number: this.number
+		})
+		this.body = `${metaData.body}`
+		this.title = metaData.title
+		this.linesChanged = metaData.additions + metaData.deletions
+		this.labels = metaData.labels.map(v => ({
+			name: v.name,
+			color: v.color,
+			description: v.description || undefined
+		}))
+		this.mergeable = metaData.mergeable ?? false
+		this.rebaseable = metaData.rebaseable ?? false
+		this.filesChanged.quantity = metaData.changed_files
+		const {data: commitData} = await this.client.rest.pulls.listCommits({
+			...context.repo,
+			pull_number: this.number
+		})
+		this.author = `${commitData[0].committer?.login}`
+		const {data: filesData} = await this.client.rest.pulls.listFiles({
+			...context.repo,
+			pull_number: this.number
+		})
+		this.filesChanged.files = filesData.map(fileData => fileData.filename)
+	}
 
-  async addLabel(label: RepoLabel) {
-    await this.client.rest.issues.addLabels({
-      ...context.repo,
-      issue_number: this.number,
-      labels: [label.name]
-    })
-  }
+	async addLabel(label: RepoLabel) {
+		await this.client.rest.issues.addLabels({
+			...context.repo,
+			issue_number: this.number,
+			labels: [label.name]
+		})
+	}
 
-  checkCondition(condition: Condition): boolean {
-    if (condition instanceof Conditions.MaxLinesCondition) {
-      return this.linesChanged < condition.maxLines
-    }
-    if (condition instanceof Conditions.MinLinesCondition) {
-      return this.linesChanged >= condition.minLines
-    }
-    if (condition instanceof Conditions.MaxFilesCondition) {
-      return this.linesChanged < condition.maxFiles
-    }
-    if (condition instanceof Conditions.MinFilesCondition) {
-      return this.linesChanged >= condition.minFiles
-    }
-    return false
-  }
+	private checkCondition(condition: Condition): boolean {
+		if (condition instanceof Conditions.MaxLinesCondition) {
+			return this.linesChanged < condition.maxLines
+		}
+		if (condition instanceof Conditions.MinLinesCondition) {
+			return this.linesChanged >= condition.minLines
+		}
+		if (condition instanceof Conditions.MaxFilesCondition) {
+			return this.linesChanged < condition.maxFiles
+		}
+		if (condition instanceof Conditions.MinFilesCondition) {
+			return this.linesChanged >= condition.minFiles
+		}
+		return false
+	}
 
-  async apply(config: ConditionalLabel[]) {
-    for (const label of config) {
-      if (label.conditions.every(this.checkCondition)) {
-        await this.addLabel(label as RepoLabel)
-      }
-    }
-  }
+	async apply(config: ConditionalLabel[]) {
+		for (const label of config) {
+			if (label.conditions.every(condition => this.checkCondition(condition))) {
+				await this.addLabel(label as RepoLabel)
+			}
+		}
+	}
 }
